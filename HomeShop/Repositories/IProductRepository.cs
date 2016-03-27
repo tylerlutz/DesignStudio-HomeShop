ce83074c1fc42e6dd9f0623a9197e72030e51199
Tyler Lutz <tlutz.computers@gmail.com>
2016-03-27 19:47:32 -0400

    Created Product Repo

---
+++ HomeShop/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using HomeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace HomeShop.Repositories
+{
+    interface IProductRepository:IDisposable
+    {
+        IQueryable<Product> All { get; }
+        IQueryable<Product> AllIncluding(params Expression<Func<Product, object>>[] includeProperties);
+        Product Find(int id);
+    }
+}
---
+++ HomeShop/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using HomeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace HomeShop.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private HomeStoreEntities context = new HomeStoreEntities();
+
+        public IQueryable<Product> All
+        {
+            get
+            {
+                return context.Products;
+            }
+        }
+
+        public IQueryable<Product> AllIncluding(params Expression<Func<Product,object>>[] includeProperties)
+        {
+            IQueryable<Product> query = context.Products;
+            foreach(var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+            return query;
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public Product Find(int id)
+        {
+            return context.Products.Find(id);
+        }
+    }
+}
