30f1c55b12d28d4d11a5ca175cc3f2d6b30cb50b
Jerimy Tate <jerimy.tate@gmail.com>
2016-04-21 20:57:35 -0400

    Created Shopping Cart Button on main menu

--- HomeShop/Controllers/HomeController.cs
+++ HomeShop/Controllers/HomeController.cs
@@ -15,14 +15,21 @@ namespace HomeShop.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "this is homestore.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "find us, meet us, greet us";
+
+            return View();
+        }
+
+        public ActionResult ShoppingCart()
+        {
+            ViewBag.Message = "our shopping cart";
 
             return View();
         }
