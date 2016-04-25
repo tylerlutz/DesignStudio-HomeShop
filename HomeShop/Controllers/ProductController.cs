86b3b78504b1f92738c890c320b27d6074455f87
Tyler Lutz <tlutz.computers@gmail.com>
2016-04-25 12:27:16 -0400

    Readded ProductController and created views

---
+++ HomeShop/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeShop.Models;
+
+namespace HomeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        HomeStoreEntities db = new HomeStoreEntities();
+
+        public ActionResult Bathroom()
+        {
+            Category category = db.Categories.FirstOrDefault(c => c.CategoryName == "Bathroom");
+            var products = db.Products.Where(p => p.CategoryID == category.CategoryID);
+
+            return View(products.ToList());
+        }
+
+        public ActionResult Kitchen()
+        {
+            Category category = db.Categories.FirstOrDefault(c => c.CategoryName == "Kitchen");
+            var products = db.Products.Where(p => p.CategoryID == category.CategoryID);
+
+            return View(products.ToList());
+        }
+
+        public ActionResult Bedroom()
+        {
+            Category category = db.Categories.FirstOrDefault(c => c.CategoryName == "Bedroom");
+            var products = db.Products.Where(p => p.CategoryID == category.CategoryID);
+
+            return View(products.ToList());
+        }
+    }
+}
