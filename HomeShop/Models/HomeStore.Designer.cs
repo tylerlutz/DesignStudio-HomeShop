55f74cc13bb03c353cab90d4c2ce99c0271ceaa2
Jerimy Tate <jerimy.tate@gmail.com>
2016-04-27 00:35:04 -0400

    Bugfixes from Merge

--- HomeShop/Models/HomeStore.Designer.cs
+++ HomeShop/Models/HomeStore.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Tyler.LUTZ\Documents\GitHub\Websites\HomeShop\HomeShop\Models\HomeStore.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\JT\Source\Repos\DesignStudio-HomeShop2\HomeShop\Models\HomeStore.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
